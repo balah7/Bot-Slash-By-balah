6b256a33cc7f87728bd866749d71e00d1b66cd99
Balah <104644967+balah7@users.noreply.github.com>
2022-07-02 20:31:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js"); 
+const client = new Discord.Client({intents: 32767});
+const config = require("./config.json"); 
+
+client.login(config.token); //Aonde o bot vai ligar
+
+client.once('ready', async () => {
+
+    console.log(`⚙ Estou ativo em ${client.user.tag}`)
+
+})
+
+//Handler
+module.exports = client;
+client.commands = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+client.config = require("./config.json");
+require("./handler")(client);
+const { glob } = require("glob");
+const { promisify } = require("util");
+
+const globPromise = promisify(glob);
+
+client.on("interactionCreate", async (interaction) => {
+
+    if (!interaction.guild) return;
+  
+    if (interaction.isCommand()) {
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+
+        if (!cmd)
+            return;
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+
+        cmd.run(client, interaction, args);
+    }
+
+    if (interaction.isContextMenu()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
+        
+    }
+});
+
